721a4a34163694cb91ccc8c37e45100b2cf0a7e4
haya-rahamasif <49449681+aquaseals@users.noreply.github.com>
2025-06-25 15:35:32 +0000

    updating tab list on delete/add tab

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,27 @@
 console.log(`this is a popup`)
 
-chrome.tabs.onCreated.addListener((tab) => {
+let currentTabs =
 
-    chrome.tabs.query({}, function(tabs) {
+chrome.tabs.query({}, function(tabs) {
     for (let i=0; i<tabs.length; i++) {
             console.log(tabs[i].title)
+            currentTabs.append(tabs[i].title)
     }
     })
 
+chrome.tabs.onCreated.addListener((tab) => {
+    currentTabs.append(tab.title)
+    console.log(currentTabs)
 })
+
+chrome.tabs.onRemoved.addListener((tab) => {
+    for (let i=0; i<currentTabs.length; i++) {
+        if(currentTabs[i] == tab.title) {
+            currentTabs.splice(i, 1)
+        }
+    }
+    console.log(currentTabs)
+})
+
+chrome.tabs.on
 
