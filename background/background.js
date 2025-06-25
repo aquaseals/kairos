554beb7679578907eea5168019cb728244123170
haya-rahamasif <49449681+aquaseals@users.noreply.github.com>
2025-06-25 23:16:38 +0000

    removing tabs hehe

--- background/background.js
+++ background/background.js
@@ -1,6 +1,7 @@
 console.log(`hello from background`)
 let seconds;
 let timeLeft;
+let deleteTabId;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -15,16 +16,27 @@ chrome.runtime.onMessage.addListener(
     }
 )
 
-function closeTab() {
+function closeTab(deleteTabId) {
     chrome.tabs.onActivated.removeListener(arguments.callee)
     chrome.tabs.onUpdated.removeListener(arguments.callee)
     chrome.windows.create({focused: true, height: 300, left: 500, top: 500, type:"popup", width: 300}, function(){
         chrome.tabs.create({url: chrome.runtime.getURL('./other/breakEndPopup.html')})
     })
+    chrome.tabs.remove(deleteTabId)
+    //chrome.runtime.sendMessage()
 }
 
 function handleBreak(selectedTab, selectedTabId, duration) {
     timeLeft = duration*60 // convert mins to seconds
+    deleteTabId = selectedTabId
+    chrome.tabs.query({active: true}, function(tab){
+        if(tab.id == selectedTabId && timeLeft > 0) {
+            startTimer()
+            console.log(`starting timer`)
+        } else {
+            pauseTimer()
+        }
+    })
     chrome.tabs.onActivated.addListener(function(activeInfo){
         if(activeInfo.tabId == selectedTabId && timeLeft > 0) {
             startTimer()
@@ -49,7 +61,7 @@ function startTimer() {
 
         if(timeLeft <= 0){
             clearInterval(timer)
-            closeTab()
+            closeTab(deleteTabId)
         }
     }, 1000)
 
