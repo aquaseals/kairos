fcdec0a4d3268cff24790eba05ba46b1cd3fdcc5
haya-rahamasif <49449681+aquaseals@users.noreply.github.com>
2025-06-25 15:01:15 +0000

    get title of tab

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,9 @@
 console.log(`this is a popup`)
 
 chrome.tabs.query({}, function(tabs) {
-    console.log(tabs)
+    console.log(tabs.title)
 })
 
-function entrance() {
-    console.log(openTabs)
-}
+
 
 entrance()
