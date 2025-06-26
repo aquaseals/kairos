76d6c1c3dd7f1f6803f9d1b676e4d5e6d154340d
haya-rahamasif <49449681+aquaseals@users.noreply.github.com>
2025-06-26 00:46:06 +0000

    no escaping the focus popup hehe

--- other/breakEndPopup.js
+++ other/breakEndPopup.js
@@ -6,7 +6,38 @@ chrome.runtime.onMessage.addListener(
         if(request.message == "goFocus"){
             currentTabs = request.currentTabs
             currentTabsIds = request.currentTabsIds
+            let focusPopup = chrome.tabs.query({active: true, title: "Focus time!"})[0].id
             console.log(currentTabs, currentTabsIds)
+
+            for (let i=0; i<currentTabs.length; i++) {
+            let tab = document.createElement("option")
+            tab.innerHTML = currentTabs[i]
+            tabDropdown.appendChild(tab)
+             }
+
+            chrome.tabs.onRemoved.addListener(function(tabId){
+                if(tabId == focusPopup) {
+                    chrome.windows.create({focused: true, height: 300, left: 500, top: 500, type:"popup", width: 300}, function(){
+                        chrome.tabs.create({url: chrome.runtime.getURL('./other/breakEndPopup.html')})
+                    })
+                }
+            })
         }
     }
 )
+
+/*
+
+chrome.windows.create({focused: true, height: 300, left: 500, top: 500, type:"popup", width: 300}, function(){ // create 2nd popup window
+        chrome.tabs.create({url: chrome.runtime.getURL('./other/breakEndPopup.html')}, function(tab){ // load popup html in new tab
+            chrome.tabs.onUpdated.addListener(function listener(tabId, inof){ // make sure popup window loaded
+                if(tabId === tab.id && inof.status == "complete") {
+                    chrome.tabs.onUpdated.removeListener(listener)
+                    chrome.tabs.sendMessage(tab.id, {message: "goFocus", currentTabs: currentTabs, currentTabsIds: currentTabsIds}) //send msg to 2nd popup
+
+                }
+            })
+        })
+    })
+
+*/
